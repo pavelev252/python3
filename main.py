b48391a1d338027da9406d3bd5dfc8f289431b14
pavelev252 <105489081+pavelev252@users.noreply.github.com>
2024-11-03 15:20:39 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(99.9 > 99.8 and 1000 != 1000.1)
+
+# 3rd program
+a = 2 * 2 + 2
+b = 2 * (2+2)
+c= a == b
+
+print(a)
+print(b)
+print(c)
+
+# 4th program
+a = '123.456'
+a = (int(float(a) * 10))
+print(a%10)
